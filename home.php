29bae366a08b13da5ac7c528d81a7fd5306313fa
Hostel Rentie <hostelrentie@gmail.com>
2021-11-04 13:28:15 +0100

    The lesson file of How to make Semantic and SEO friendly url with .htaccess file youtube tutorial

---
+++ home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Semantic URL</title>
+</head>
+<body>
+    <h1 style="text-align: center">Semantic and SEO friendly URL with .htaccess file</h1>
+    <a href="another-page?post-year=2021&post-month=11&post-str=how-to-make-seo-friendly-url">
+        Go to another page this os wrong ---> 
+        another-page?post-year=2021&post-month=11&post-str=how-to-make-seo-friendly-url
+    </a>
+    <br>
+    <a href="another-page/how-to-make-seo-friendly-url/11/2021">another-page/how-to-make-seo-friendly-url/11/2021</a>
+</body>
+</html>
